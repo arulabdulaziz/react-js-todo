9406fae90e34e2bb0b09cc4178c237ac2d6cb7ba
arulabdulaziz <rulz.matrixs@gmail.com>
2021-01-04 05:21:14 +0700

    1. baru  bisa handle 1 input 2. belum menggunakan routing

---
+++ src/ListItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ListItem = (props) => {
+  return <li className="list-group-item">
+    <button
+      className="btn-sm mr-4 btn btn-info"
+      onClick={props.editTodo}
+    >Update</button>
+    {props.item.todo}
+    <button
+      className="btn-sm ml-4 btn btn-danger"
+      onClick={props.deleteTodo}
+    >Delete</button>
+  </li>;
+};
+
+export default ListItem;
